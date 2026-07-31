12b82437b68d11c0a48b821da70cb80dfca3d233
Praneethvemula <vemulapraneeth108@gmail.com>
2026-07-31 18:53:14 +0530

    initial commit

---
+++ variableExample.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+ // Allows us to use cout without std::
+using namespace std;
+int main(){
+    // Creates an integer variable named apple and stores 50
+    int apple=50;
+    // Creates a character array to store the name
+    char name[]="praneeth";
+    cout << "The cost of apple is:"<<apple<<endl;
+    cout <<"The name of Person is:"<< name <<endl;
+    return 0;
+}
