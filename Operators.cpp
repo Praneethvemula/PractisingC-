8f321bedf8c930515c83c2d3ccd064ccacd9fb67
Vemula Praneeth <vemulapraneeth108@gmail.com>
2026-08-03 22:08:17 +0530

    Add files via upload

---
+++ Operators.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i, j,add,sub,mul,div,rem;
+
+    i = 13;
+    j = 5;
+    add=i+j;
+    sub=i-j;
+    mul=i*j;
+    div=i/j;
+    rem= i % j;
+    
+    cout<<"addition:"<<add<<endl;
+    cout<<"subtraction:"<<sub<<endl;
+    cout<<"multiplication:"<<mul<<endl;
+    cout<<"division:"<<div<<endl;
+    cout<<"remainder:"<<rem<<endl;
+    
+}
---
+++ floatingpoint.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i;
+float f;
+i = 100;
+f = i;
+    cout<<"the value of i is:"<<i<<endl;
+    cout<<"the value of f is:"<<f<<endl;
+   
+}
