77ba9830ebb3c7303df4f9cd4905f4fd8aceece3
Praneethvemula <vemulapraneeth108@gmail.com>
2026-08-12 22:00:52 +0530

    Third changes

---
+++ assignmentope.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 15;
+
+    x += 5;
+    cout << x << endl;
+
+    x -= 3;
+    cout << x << endl;
+
+    x *= 2;
+    cout << x << endl;
+
+    x /= 4;
+    cout << x << endl;
+
+    return 0;
+}
