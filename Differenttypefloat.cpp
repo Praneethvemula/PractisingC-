e801827569ff00ab09e3d07ffb1fa9714a9bf513
Praneethvemula <vemulapraneeth108@gmail.com>
2026-08-14 21:54:05 +0530

    This is my fourth commit

---
+++ Differenttypefloat.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+ int main(){
+    cout<<"The size of float is:"<<sizeof(float)<<"bytes"<<endl;
+    cout<<"The size of double is:"<<sizeof(double)<<"bytes"<<endl;
+    cout<<"The size of long double is:"<<sizeof(long double)<<"bytes"<<endl;
+    return 0;
+ }
---
+++ Differenttypeint.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"The size of int is:"<<sizeof(int)<<"bytes"<<endl;
+    cout<<"The size of long int is:"<<sizeof(long int)<<"bytes"<<endl;
+    cout<<"The size of short int is:"<<sizeof(short int)<<"bytes"<<endl;
+    cout<<"the size of signed int:"<<sizeof(signed int)<<"bytes"<<endl;
+    cout<<"The size of unsigned int is:"<<sizeof(unsigned int)<<"bytes"<<endl;
+    cout<<"The size of the long long int is:"<<sizeof(long long int)<<"bytes"<<endl;
+    return 0;
+}
